3447ccac063fe25e1eaef74bd2623e5eba47755c
Morten Nilsen <morten@runsafe.no>
2013-12-15 13:02:58 +0100

    Declare features used

--- src/no/runsafe/runsafepexbridge/Plugin.java
+++ src/no/runsafe/runsafepexbridge/Plugin.java
@@ -1,12 +1,19 @@
 package no.runsafe.runsafepexbridge;
 
 import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.features.Events;
+import no.runsafe.framework.features.FrameworkHooks;
 
 public class Plugin extends RunsafePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		// Framework features
+		addComponent(Events.class);
+		addComponent(FrameworkHooks.class);
+
+		// Plugin components
 		addComponent(PermissionsExWrapper.class);
 	}
 }
